1ae1c68a13c1be5e1e3b0f67c597b7fc433bb902
Hieu <stsbto@gmail.com>
2022-10-26 23:17:45 +0700

    clean main

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 const cookieSession = require('cookie-session');
 
-// import { CookieSession } from 'cookie-session';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieSession({ keys: ['asdfasdf'] }));
